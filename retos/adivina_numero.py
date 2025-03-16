866888908381eedc9e528d87e5c16755b407ec11
Brayan Daniel Perez Pinto <brayanperezpinto.gmail.com>
2025-03-16 11:34:08 -0600

    Primer_reto

--- retos/adivina_numero.py
+++ retos/adivina_numero.py
@@ -3,18 +3,28 @@ if __name__ == '__main__':
     i: int = 0
     nuRandom = random.randint(1,100)
     bol = False
-    while bol == False:
-        y = int(input("Ingresa un numero entero entre 1 y 100: "))
-        if y < nuRandom:
-            print(f"Es menor tu numero que el numero a adivinar")
-            i = +1
-        else:
-            if y>nuRandom:
-                print(f"Es mayor tu numero que el numero a adivinar")
-                i = +1
+    while bol == False and i<10:
+        try:
+            y = int(input("Ingresa un numero entero entre 1 y 100: "))
+            if y >= 1 and y <= 100:
+                i += 1
+                if y < nuRandom:
+                    print("El numero a divinar es mayor")
+                else:
+                    if y > nuRandom:
+                        print("El numero a adivinar es menor")
+                    else:
+                        if y == nuRandom:
+                            print("Felicidades has acertado el numero")
+                            print(f"Lo lograste en el intento numero : {i} ")
+                            bol = True
+                if i==10:
+                    print(f"No tienes mas intentos. El numero era {nuRandom}")
             else:
-                if y==nuRandom:
-                    print("Felicidades has acertado el numero")
-                    print(f"Lo has intentado : {i} veces")
-                    bol = True
+                print("Debe estar en el rango entre 1 y 100")
+                print("Intentalo nuevamente")
+
+        except ValueError:
+            print("Ingrese un numero entero")
+
 
